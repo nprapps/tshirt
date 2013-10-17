867a3a145a3e2b469463d928a202a5fd4b00bdae
lindamood <wesley.lindamood@gmail.com>
2013-10-17 10:40:47 -0400

    adding vimeo api controlled video to template

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,38 @@
-$(function() {
+$(document).ready(function() {
+	
+	//video 1
+	var $iframe = $('#video')[0];
+	var $player = $f($iframe);
+	
+	//video 2
+	var $iframe1 = $('#video1')[0];
+	var $player1 = $f($iframe1);
+	
+	$("#video-wrapper").fitVids();
+	
+	$player.addEvent('ready', function() {
+		//console.log('player ready');
+		//$player.api('setVolume', 0);
+		//$player1.api('play');
+		//$player1.api('seekTo', 3);
+		//$player1.api('pause');
+	});
+	
+	$( "#play" ).on( "click", function() {
+		$player.api('play');
+		$("#video-wrapper").addClass("animated fadeIn backer" );
+	});
+	
+	$( "#pause" ).on( "click", function() {
+		$player.api('pause');
+	});
+	
+	$( "#mute" ).on( "click", function() {
+		$player.api('setVolume', 0);
+	});
+	
+	$( "#play1" ).on( "click", function() {
+		$player1.api('play');
+	});
 
 });
