8478485f017c83fe34eae50240f282d167bd1158
Alyson Hurt <ahurt@npr.org>
2013-11-19 15:21:59 -0500

    kill cotton exports chart

--- www/js/app.js
+++ www/js/app.js
@@ -43,10 +43,8 @@ $(document).ready(function() {
 
     // data vars
     var $d3_apparel_wages = $('#apparel-wages-d3');
-    var $d3_cotton_exports = $('#cotton-exports-d3');
     var $d3_tshirt_phase = $('#tshirt-phase-d3');
     var d3_apparel_wages_data;
-    var d3_cotton_exports_data;
     var d3_tshirt_phase_data;
     
     //breakpoints
@@ -512,29 +510,6 @@ $(document).ready(function() {
             }
         });
 
-        d3.tsv("data/cotton-exports.tsv", function(error, data) {
-            d3_cotton_exports_data = data;
-            d3_cotton_exports_data.forEach(function(d) {
-                d.year = d3.time.format('%Y').parse(d.year);
-            });
-            if (current_chapter == 'cotton') {
-                draw_cotton_exports_graph();
-            }
-        });
-
-/*
-        d3.csv("data/tshirt-phase.csv", function(error, data) {
-            d3_tshirt_phase_data = data;
-            d3_tshirt_phase_data.forEach(function(d) {
-                d.year = d3.time.format('%Y').parse(d.year);
-            });
-            if (current_chapter == 'people') {
-                console.log('draw_tshirt_phase_graph!'); 
-                draw_tshirt_phase_graph();
-            }
-        });
-*/
-
         d3.csv("data/tshirt-phase.csv", function(error, data) {
             d3_tshirt_phase_data = data;
             d3_tshirt_phase_data.forEach(function(d) {
@@ -629,117 +604,6 @@ $(document).ready(function() {
                 .attr('text-anchor', 'end')
                 .attr('class', function(d) { return d.country.toLowerCase() })
                 .text(function(d) { return d.country });
-    }
-    
-    function draw_cotton_exports_graph() {
-        var margin = {top: 0, right: 15, bottom: 25, left: 50};
-        var width = $d3_cotton_exports.width() - margin.left - margin.right;
-        var height = Math.ceil((width * graphic_aspect_height) / graphic_aspect_width) - margin.top - margin.bottom;
-
-        // clear out existing graphics
-        reset_charts('cotton-exports');
-
-        // remove placeholder image if it exists
-        $d3_cotton_exports.find('img').remove();
-
-        var x = d3.time.scale()
-            .range([0, width]);
-
-        var y = d3.scale.linear()
-            .range([height, 0]);
-
-        var color = d3.scale.category10()
-            .domain(d3.keys(d3_cotton_exports_data[0]).filter(function(key) { return key !== 'year'; }));
-        // more: https://github.com/mbostock/d3/wiki/Ordinal-Scales#wiki-category10
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom")
-            .ticks(5);
-            
-        var x_axis_grid = function() { return xAxis; }
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
-        
-        var y_axis_grid = function() { return yAxis; }
-        
-        var line = d3.svg.line()
-            .x(function(d) { return x(d.year); })
-            .y(function(d) { return y(d.exports); });
-        
-        var legend = d3.select('#cotton-exports-d3').append('ul')
-                .attr('class', 'key')
-            .selectAll('g')
-                .data(color.domain().slice())
-            .enter().append('li')
-                .attr('class', function(d, i) { return 'key-item key-' + i; });
-
-        legend.append('b')
-            .style('background-color', color);
-
-        legend.append('label')
-            .text(function(d) { return d; });
-
-        var svg = d3.select('#cotton-exports-d3').append('svg')
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            
-        var countries = color.domain().map(function(country) {
-            return {
-                country: country,
-                values: d3_cotton_exports_data.map(function(d) {
-                    return {
-                        year: d.year, 
-                        exports: +d[country]
-                    };
-                })
-            };
-        });
-
-        x.domain(d3.extent(d3_cotton_exports_data, function(d) { return d.year; }));
-
-        y.domain([
-            d3.min(countries, function(c) { return d3.min(c.values, function(v) { return v.exports; }); }),
-            d3.max(countries, function(c) { return d3.max(c.values, function(v) { return v.exports; }); })
-        ]);
-
-        svg.append('g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(xAxis);
-
-        svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis);
-
-        svg.append('g')
-            .attr('class', 'x grid')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(x_axis_grid()
-                .tickSize(-height, 0, 0)
-                .tickFormat('')
-            );
-
-        svg.append('g')
-            .attr('class', 'y grid')
-            .call(y_axis_grid()
-                .tickSize(-width, 0, 0)
-                .tickFormat('')
-            );
-
-        var country = svg.selectAll('.country')
-            .data(countries)
-            .enter().append('g')
-            .attr('class', 'country');
-
-        country.append('path')
-            .attr('class', 'line')
-            .attr('d', function(d) { return line(d.values); })
-            .style('stroke', function(d) { return color(d.country); });
     }
     
     function draw_tshirt_phase_graph() {
@@ -859,11 +723,6 @@ $(document).ready(function() {
         // 1) the current chapter 
         // 2) if the data has actually loaded
 	    switch(current_chapter) {
-	        case 'cotton':
-	            if (d3_cotton_exports_data != undefined) {
-                    draw_cotton_exports_graph();
-                }
-	            break;
 	        case 'people':
 	            if (d3_apparel_wages_data != undefined) {
                     draw_apparel_wages_graph();
@@ -876,11 +735,6 @@ $(document).ready(function() {
     }
     
     function reset_charts(caller) {
-        if (d3.select('#cotton-exports-d3').select('svg')[0][0] != null) {
-            d3.select('#cotton-exports-d3').selectAll('svg').remove();
-            d3.select('#cotton-exports-d3').selectAll('.key').remove();
-        }
-
         if (caller != 'tshirt-phase') {
         // ^ clumsy, but i'm trying to keep graphs on the same page from zeroing each other out
             if (d3.select('#apparel-wages-d3').select('svg')[0][0] != null) {
