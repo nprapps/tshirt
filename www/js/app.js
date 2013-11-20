98cc46987b2a62cda66b3eda08ad58d437107527
lindamood <wesley.lindamood@gmail.com>
2013-11-20 14:11:44 -0500

    addign a slight fade to title card text

--- www/js/app.js
+++ www/js/app.js
@@ -183,7 +183,7 @@ $(document).ready(function() {
 	            if (COPY[chapter]['loop_video_mp4'].length > 0 && !is_touch && supports_html5_video) { // desktops only
 	                var video_tag = '';
 	        
-	                video_tag += '<video class="title-video" preload="auto" loop="">';
+	                video_tag += '<video class="title-video animated" preload="auto" loop="">';
 	                video_tag += '<source src="' + COPY[chapter]['loop_video_mp4'] + '" type="video/mp4">';
 	                video_tag += '<source src="' + COPY[chapter]['loop_video_webm'] + '" type="video/webm">';
 	                video_tag += '</video>';
