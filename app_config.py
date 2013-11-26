aa7dd49faad35ee4c6f19ae502149c9d8ab31562
Alyson Hurt <ahurt@npr.org>
2013-11-25 20:14:10 -0500

    social thumb for FB/twitter. closes #337

--- app_config.py
+++ app_config.py
@@ -91,7 +91,7 @@ TWITTER = {
     'TEXT': PROJECT_NAME,
     'URL': SHARE_URL,
     # Will be resized to 120x120, can't be larger than 1MB
-    'IMAGE_URL': ''
+    'IMAGE_URL': 'http://apps.npr.org/tshirt/img/social-thumb.jpg'
 }
 
 FACEBOOK = {
