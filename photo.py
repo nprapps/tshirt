42018064f44927c36b1f4d22b32d85ccf0a3f03f
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-11-26 10:22:20 -0500

    Working photo importer for S2S.

--- photo.py
+++ photo.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+from bs4 import BeautifulSoup
 import csv
 import json
 
 import requests
 
-FIELDNAMES = ['date', 'name', 'caption', 'url', 'img', 'moderated']
+FIELDNAMES = ['date', 'name', 'caption', 'url', 'preview', 'remote_img_url', 'moderated']
 
 class Photo(object):
 
@@ -13,6 +14,7 @@ class Photo(object):
         for field in FIELDNAMES:
             if kwargs[field]:
                 setattr(self, field, kwargs[field])
+        setattr(self, 'local_img_url', self.remote_img_url.split(u's3.amazonaws.com/')[1].replace('_8.jpg', '.jpg'))
 
 def get_photo_csv():
     r = requests.get('https://docs.google.com/spreadsheet/pub?key=0Aga89cI2jWk5dFdqZmp0YXQ5RDBNWlNaTV90cXFYQWc&output=csv')
@@ -23,13 +25,24 @@ def parse_photo_csv():
     with open('data/photos.csv', 'rb') as readfile:
         photos = list(csv.DictReader(readfile, fieldnames=FIELDNAMES))
 
+    print "Processing %s entries from Google Docs." % len(photos)
+
     payload = []
 
     for photo in photos:
         if photo['moderated'] != '':
-            p = Photo(**photo)
-            payload.append(p.__dict__)
+            r = requests.get(photo['remote_img_url'])
+            if r.status_code == 200:
+                p = Photo(**photo)
+                payload.append(p.__dict__)
+                with open('www/img/s2s-instagram-%s' % p.local_img_url, 'wb') as writefile:
+                    writefile.write(r.content)
+            else:
+                print "! Error: Photo %s does not have an image file." % (photo['url'])
+        else:
+            print "* Unmoderated: Photo %s has been unmoderated." % (photo['url'])
 
+    print "Writing %s photos to JSON." % len(payload)
     with open('www/data/photos.json', 'wb') as writefile:
         writefile.write(json.dumps(payload))
 
